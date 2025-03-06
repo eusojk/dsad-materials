abefada363c21692c576981a4e5d40760bd0bfd1
Josué <josue.kpodo@gmail.com>
2025-03-06 08:57:12 -0500

    Refactor main function to use LinkedList class and implement push method for head-first insertion

--- 05-linked-lists/projects/starter/bin/starter.dart
+++ 05-linked-lists/projects/starter/bin/starter.dart
@@ -1,12 +1,10 @@
 import 'package:starter/linked_list.dart';
 
 void main() {
-  final node1 = Node(value: 1);
-  final node2 = Node(value: 2);
-  final node3 = Node(value: 3);
+  final list = LinkedList();
+  list.push(3);
+  list.push(2);
+  list.push(1);
 
-  node1.next = node2;
-  node2.next = node3;
-
-  print(node1);
+  print(list);
 }
--- 05-linked-lists/projects/starter/lib/linked_list.dart
+++ 05-linked-lists/projects/starter/lib/linked_list.dart
@@ -28,4 +28,12 @@ class LinkedList<E> {
     if (isEmpty) return 'Empty list';
     return head.toString();
   }
+
+  // pushing to the front of a list aka head-first insertion
+  void push(E value) {
+    // create a new node and point to the node that used to be head & set it as head
+    head = Node(value: value, next: head);
+    // in case you’re pushing into an empty list, the new node is both the head and tail of the list.
+    tail ??= head;
+  }
 }
